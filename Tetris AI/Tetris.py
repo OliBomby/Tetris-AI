6945c289f2e9b2601a664ef849a53b53c9e3c721
OliBomby <olivierschipper@gmail.com>
2018-09-25 19:25:19 +0200

    Reform + stolen tetris clone
    the tetris clone has to be changed

--- Tetris AI/Tetris.py
+++ Tetris AI/Tetris.py
@@ -1,9 +1,28 @@
+import copy
+
 import numpy as np
 
 WIDTH = 10
-HEIGHT = 30
+HEIGHT = 40
 
 PLAYING_FIELD_DIMENSIONS = (WIDTH, HEIGHT)
+NUM_UPCOMING = 4
+NUM_TETROMINO = 7
+
+TETROMINO = []
+
+class State:
+    def __init__(self, fill=True):
+        self.field = np.zeros(PLAYING_FIELD_DIMENSIONS, dtype=np.float32)
+        self.active = np.zeros(NUM_TETROMINO, dtype=np.float32)
+        self.active_pos = [0, 0]
+        self.active_rot = 0
+        self.hold = np.zeros(NUM_TETROMINO, dtype=np.float32)
+        self.upcoming = np.zeros((NUM_UPCOMING, NUM_TETROMINO), dtype=np.float32)
+
+        if fill:
+            self.active[np.random.choice(NUM_TETROMINO)] = 1
+            self.upcoming = np.eye(NUM_TETROMINO)[np.random.choice(NUM_TETROMINO, NUM_UPCOMING)]
 
 
 def detect_full_rows(field):
@@ -13,9 +32,10 @@ def detect_full_rows(field):
 def get_next_game_state(state, game_input=None):
     if game_input is None:
         game_input = [0 for x in range(5)]
-    field = state[0]
 
-    index_full_rows = detect_full_rows(field)
+    state = copy.deepcopy(state)
+
+    index_full_rows = detect_full_rows(state)
     num_full_rows = len(index_full_rows)
     # move piece down
     # respond to input
@@ -23,6 +43,7 @@ def get_next_game_state(state, game_input=None):
     # do stuff
     # cycle pieces or something
     # generate new state
+    return state
 
 
 def draw_game_state(state):
@@ -30,19 +51,21 @@ def draw_game_state(state):
     pass
 
 
+def check_inputs():
+    pass
+
+
 def game_loop():
     # artificial loop for human play
 
-    field = np.zeros(PLAYING_FIELD_DIMENSIONS, dtype=np.float32)
-
-
-
-    state = (field)
+    state = State()
 
     while True:
         # check inputs
+        game_input = check_inputs()
         # get_next_game_state
+        state = get_next_game_state(state, game_input)
         # draw_game_state
-        pass
+        draw_game_state(state)
 
-
+game_loop()
